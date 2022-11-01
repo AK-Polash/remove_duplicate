ed5f53809a5e017d849380c91bd777704ac18359
AK-Polash <polashk199@gmail.com>
2022-11-01 11:12:33 +0600

    project completed

--- custom.js
+++ custom.js
@@ -1,12 +1,16 @@
+let input = document.querySelector("#input");
+let button = document.querySelector(".button");
 let heading = document.querySelector(".heading");
-let myArr = [1, 1, 2, 3, "apple", "orange", "banana", "apple"];
-heading.innerHTML = "The filtered array : ";
+let myArr = [];
 
-heading.innerHTML += myArr.filter((item, index) => {
-  console.log(item);
-  return myArr.indexOf(item) === index; // ei line ta bujhi nai ( return keno holo + index er sathe compare kore ki kortese eikhane?)
+button.addEventListener("click", () => {
+  myArr.push(input.value);
+  input.value = "";
+
+  myArr = myArr.filter((item, index) => {
+    return myArr.indexOf(item) === index;
+  });
+
+  heading.innerHTML = myArr.join(" ");
+  console.log(myArr);
 });
-
-console.log(myArr);
-
-// eikhane 'myArr' change hocce na keno? eita primitive data type er moto behave kortese keno?
